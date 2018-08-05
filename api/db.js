9031d1e42b229a2c5bf298605b50a193259caf61
Igor Rotondo Bagliotti <igor.bagliotti@gmail.com>
2018-08-05 17:24:42 -0300

    Corrigindo deprection of mongoose

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://dev:dev@ds145848.mlab.com:45848/mongo', { useMongoClient: true });
 
 const db = mongoose.connection;
